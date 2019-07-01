5f00066aadfaeffadbcd589d4f1a42356e24ec2e
rebecaneris <52168578+rebecaneris@users.noreply.github.com>
2019-07-01 17:14:50 -0300

    Add files via upload

---
+++ 1bit_Adder.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/23/2019 10:54:43 AM
+-- Design Name: 
+-- Module Name: 1bit_Adder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Adder_1bit is
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           c_in : in STD_LOGIC;
+           s : out STD_LOGIC;
+           c_out : out STD_LOGIC);
+end Adder_1bit;
+
+architecture Behavioral of Adder_1bit is
+
+begin
+
+s <= a xor b xor c_in;
+c_out <= ((a xor b) and c_in) or (a and b) ;
+
+end Behavioral;
