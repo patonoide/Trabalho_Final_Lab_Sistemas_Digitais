49cbc5a397d7066f324fca6581bc8a77c22009f2
Rafael Barbosa <rafael.1940.b@gmail.com>
2019-07-01 20:57:45 -0300

    Adicionado Myca-2. Faltando TOP Module e ROM

---
+++ 74LS194.vhd
@@ -0,0 +1,56 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+
+
+entity D74LS194 is
+    port (
+    clk       : in  std_logic;
+    clr      : in  std_logic ;
+    S       : in std_logic_vector (1 downto 0);
+    R       : in std_logic;
+    D       : in std_logic_vector (7 downto 0);
+    L       : in std_logic;
+    Q       : out std_logic_vector (7 downto 0)
+    );
+
+end entity;
+
+architecture  deslocador of D74LS194 is
+
+    signal flip : std_logic_vector (7 downto 0);
+begin
+    process(clr,S,R,D,L, clk)
+    begin
+        if rising_edge(clk) then
+            if clr = '0' then
+                flip <= "0000";
+            end if;
+            if clr = '1' and S = "00" then
+                flip <= flip;
+            end if;
+            if clr = '1' and S = "01" and R = '0' then
+                flip <= '0' & flip(7) & flip(6) & flip(5) & flip(4) & flip(3) & flip(2) & flip(1);
+            end if;
+            if clr = '1' and S = "01" and R = '1' then
+                flip <= '1' & flip(7) & flip(6) & flip(5) & flip(4) & flip(3) & flip(2) & flip(1);
+            end if;
+            if clr = '1' and S = "10" and L = '0' then
+                flip <= flip(6) & flip(5) & flip(4) & flip(3) & flip(2) & flip(1) & flip(0) & '0' ;
+            end if;
+            if clr = '1' and S = "10" and L = '1' then
+                flip <= flip(6) & flip(5) & flip(4) & flip(2) & flip(1) & flip(0) & '1' ;
+            end if;
+            if clr = '1' and S = "11" then
+                flip <= D;
+            end if;
+
+        end if;
+
+        Q <= flip;
+
+    end process;
+
+
+
+end architecture;
---
+++ Myca_2.vhd
@@ -0,0 +1,73 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity myca2 is
+    Port ( clk : in STD_LOGIC;
+           clr : in STD_LOGIC;
+           flag : in STD_LOGIC;
+           braddr : in STD_LOGIC_VECTOR (7 downto 0);
+           opcode : in STD_LOGIC_VECTOR (3 downto 0);
+           jaddr : in STD_LOGIC_VECTOR (7 downto 0);
+           Q : out STD_LOGIC_VECTOR (7 downto 0));
+end myca2;
+
+architecture Behavioral of myca2 is
+
+signal ld, en : std_logic;
+signal deco_out, P, f, C :  std_logic_Vector (7 downto 0);
+signal D0, D1, D2, D3: STD_LOGIC_VECTOR (7 downto 0);
+signal Smux: STD_LOGIC_VECTOR (1 downto 0);
+signal Ymux: STD_LOGIC_VECTOR (7 downto 0);
+
+begin
+
+contador: entity work.C74LS169 port map(
+    clk => clk,
+    clr => clr,
+    ld => deco_out(5),
+    en => deco_out(6),
+    ud => '1',
+    D => braddr,
+    oof => open,
+    C => C
+);
+
+pilha: entity work.pilha port map(
+    clk => clk,
+    clr => clr,
+    S(0) => deco_out(1),
+    S(1) => deco_out(2),
+    A => f,
+    P => D2
+);
+
+fulladder: entity work.nbit_adder port map(
+    a => "00000000",
+    b => C,
+    cin => deco_out(0),
+    f => f
+);
+
+opdecode: entity work.opcode_decoder port map(
+    deco_in(0) => flag,
+    deco_in(1) => opcode(0),
+    deco_in(2) => opcode(1),
+    deco_in(3) => opcode(2),
+    deco_out => deco_out
+);
+Smux <= deco_out(4) & deco_out(3); 
+
+mux: process(Smux)
+begin
+    case Smux is
+    when "00" => Ymux <= braddr;
+    when "01" => Ymux <= jaddr;
+    when "10" => Ymux <= P;
+    when "11" => Ymux <= "00000000";
+    when others =>  Ymux <= "00000000";
+    end case;
+end process;
+
+Q <= C;
+
+end Behavioral;
